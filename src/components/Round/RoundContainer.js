dfdebbc55fdde0f169bd89f14a4b15fb639d4d04
Jared Smith <jaredsmith07@gmail.com>
2019-05-17 15:37:05 -0500

    improve round view styles

--- src/components/Round/RoundContainer.js
+++ src/components/Round/RoundContainer.js
@@ -5,13 +5,13 @@ class RoundContainer extends Component {
 	render() {
 		return (
 			<div className="round">
-				<div className="strategies">
+				<div className="strategies flex-row">
 					{this.props.strategies.map(strategy => (
-						<div className={"card strategy__" + strategy.name} key={strategy.id}>
+						<div className={"card card--" + strategy.color} key={strategy.id}>
 							<h2>{ strategy.name }</h2>
 							<div className="strategy__players">
 							{this.props.players.map(player => (
-								<button type="button">{ player.name }</button>
+								<button type="button" class={"button-md button--" + strategy.color}>{ player.name }</button>
 							))}
 							</div>
 						</div>
