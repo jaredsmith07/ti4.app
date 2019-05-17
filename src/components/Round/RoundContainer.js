efb7c058d9030850558f5a7a430e540e9f7420a1
Jared Smith <jaredsmith07@gmail.com>
2019-05-16 22:49:34 -0500

    start round view markup and styles

--- src/components/Round/RoundContainer.js
+++ src/components/Round/RoundContainer.js
@@ -6,8 +6,15 @@ class RoundContainer extends Component {
 		return (
 			<div className="round">
 				<div className="strategies">
-					{this.props.strategies.map( (strategy) => (
-						<div className="card"><h2>{ strategy.name }</h2></div>
+					{this.props.strategies.map(strategy => (
+						<div className={"card strategy__" + strategy.name} key={strategy.id}>
+							<h2>{ strategy.name }</h2>
+							<div className="strategy__players">
+							{this.props.players.map(player => (
+								<button type="button">{ player.name }</button>
+							))}
+							</div>
+						</div>
 					))}
 				</div>
 			</div>
--- src/components/Rules/RulesContainer.js
+++ src/components/Rules/RulesContainer.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import * as constants from '../Rules/RulesData';
 
 class RulesContainer extends Component {
 
+	componentDidMount() {
+		const ruleToggles = document.querySelectorAll('.rule__heading');
+		ruleToggles.forEach( (ruleToggle) => {
+			ruleToggle.addEventListener('click', (e) => {
+				const toggle = e.target.closest('.rule__heading');
+				const content = toggle.nextSibling;
+				content.classList.add('rule__content--open');
+			});
+		})
+	}
+
 	render() {
 		return ( 
-            <div>
-                <p>Rules reference to go here.</p>
+            <div className="rules__list">
+                {constants.rules.map( (rule) => (
+					<div className="rule" key={rule.id}>
+						<div className="rule__heading">
+							<span>{rule.id}</span>
+							<h2>{rule.title}</h2>
+						</div>
+						<div className="rule__content" dangerouslySetInnerHTML={{__html: rule.content}}></div>
+					</div>
+				))}
             </div>
 		);
 	}
