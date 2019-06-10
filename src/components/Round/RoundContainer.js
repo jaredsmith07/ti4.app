e71f9aa0c90b78b6552b8be4eb564cf28b8c5c6e
Jared Smith <jaredsmith07@gmail.com>
2019-06-10 13:09:04 -0500

    add drag n drop test

--- src/components/Round/RoundContainer.js
+++ src/components/Round/RoundContainer.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import DndTest from '../dnd/DndTest';
 
 class RoundContainer extends Component {
 
 	render() {
 		return (
 			<div className="round">
+
+				<DndTest />
+
 				<div className="strategies flex-row">
+
+					
 
 					{this.props.strategies.map( (strategy, index) => (
 						<div className={"card card--" + strategy.color} key={strategy.id}>
+							<p className="strategy__number">{ strategy.id }</p>
 							<h2>{ strategy.name }</h2>
 							<div className="strategy__players">
-
+{/* 
 							{this.props.players.map(player => (
 								<button
 									type="button"
@@ -19,7 +26,7 @@ class RoundContainer extends Component {
 									onClick={ () => this.props.pickStrat(index, player.name) }>
 									{ player.name }
 								</button>
-							))}
+							))} */}
 							
 							</div>
 						</div>
